7d239ed615680d321cb47cced3b008ab1bf83981
kimo3020 <kareem148514@bue.edu.eg>
2019-04-25 00:09:14 +0200

    DisplayClockEvent done
    DisplayMsgEvent done

---
+++ Rts/src/event/DisplayLateDose.java
@@ -0,0 +1,16 @@
+package event;
+
+public class DisplayLateDose {
+	int latedose;
+
+	public DisplayLateDose(int latedose) {
+		latedose = latedose;
+	}
+
+	public int getLateDose() {
+		return latedose;
+	}
+
+	
+
+}
--- Rts/src/model/SystemTester.java
+++ Rts/src/model/SystemTester.java
@@ -14,7 +14,7 @@ public class SystemTester {
 				while (true) {
 					Config.sendEvent(new SystemTesterEvent("OK"));
 					try {
-						Thread.sleep(10000);// wait 30 sec
+						Thread.sleep(30000);// wait 30 sec
 					} catch (InterruptedException ex) {
 						Logger.getLogger(Clock.class.getName()).log(Level.SEVERE, null, ex);
 					}

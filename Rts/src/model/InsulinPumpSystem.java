66fda2d5798620c0d2bd5e85a2ed2f37537aa174
Amr Morsy <amr905@gmail.com>
2019-04-25 00:14:29 +0200

    added insulin pump sys logic

--- Rts/src/model/InsulinPumpSystem.java
+++ Rts/src/model/InsulinPumpSystem.java
@@ -11,22 +11,36 @@ public class InsulinPumpSystem {
 	private Display MyDisplay;
 	private SugarMesurment sugarMesurment;
 	private SystemTester St;
-	private int [] sugerReading;
+	private int[] sugerReading;
 	private int ReadingIndex;
+	private int safeMin;
+	private int safeMax;
+	private int maxDailyDose;
+	private int maxSingleDose;
+	private int computedDose;
+	private float currentRate;
+	private int rateDirection;// -1 decr , 1 inc
 
 	public InsulinPumpSystem(HumanBody humanBody) {
 		this.clock = new Clock();
 		this.MyDisplay = new Display();
-		this.St=new SystemTester();
+		this.St = new SystemTester();
 		this.sugarMesurment = new SugarMesurment(humanBody);
-		sugerReading=new int[3];
-		ReadingIndex=0;
-		
+		sugerReading = new int[3];
+		ReadingIndex = 0;
+		safeMin = 70;
+		safeMax = 100;
+		maxDailyDose = 100;
+		maxSingleDose = 60;
+		computedDose = 0;
+		currentRate = 0;
+		rateDirection = 0;
 	}
 
 	public void Timer(LocalTime time) {
 		this.MyDisplay.displayClock(time);
 	}
+
 	public void SystemError(String s) {
 		this.MyDisplay.displayError(s);
 	}
@@ -36,17 +50,53 @@ public class InsulinPumpSystem {
 			Config.sendEvent(new DisplayClockEvent("ERROR CLOSING"));
 			// Stop();
 		}
-		/*else {
-			Config.sendEvent(new DisplayEvent("System Working"));
-		}*/
-			
+		/*
+		 * else { Config.sendEvent(new DisplayEvent("System Working")); }
+		 */
+
 	}
-	
+
+	private void checkSugerLevel() {
+		if ((sugerReading[ReadingIndex] > safeMax && rateDirection != 0) || (sugerReading[ReadingIndex] >= safeMin
+				&& sugerReading[ReadingIndex] <= safeMax && rateDirection == 0)) {
+			int dose = computeDose();
+			computedDose += dose;
+			// insluin pump please
+		}
+
+	}
+
+	private void calcRate() {
+		int rate1 = sugerReading[2] - sugerReading[1];
+		int rate2 = sugerReading[1] - sugerReading[0];
+		if (rate1 > rate2)
+			rateDirection = 1;// incr
+		else if (rate1 > rate2)
+			rateDirection = -1;// dec
+		else
+			rateDirection = 0;// same
+
+		currentRate = (rate1 + rate2) / 2;
+	}
+
+	private int computeDose() {
+		int dose = sugerReading[ReadingIndex] - safeMax;
+		if (dose > maxSingleDose)
+			dose = maxSingleDose;
+		if (computedDose + dose > maxDailyDose)
+			dose = maxDailyDose - computedDose;
+
+		return dose;
+
+	}
+
 	public void addSugerMeasure(int sugerReading) {
-		this.sugerReading[ReadingIndex]=sugerReading;
-		ReadingIndex=(ReadingIndex+1)%3;
-		//if(ReadingIndex==0)
-			//do something
-		
+		this.sugerReading[ReadingIndex] = sugerReading;
+
+		checkSugerLevel();
+		if (ReadingIndex == 2)
+			calcRate();
+
+		ReadingIndex = (ReadingIndex + 1) % 3;
 	}
 }
